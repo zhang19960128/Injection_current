ff0e7087f89cd67d0888b68507f7c996cf5432db
zhang19960128 <zjh199601283492@gmail.com>
2020-01-21 13:46:24 -0500

    change

--- src/indexref.cpp
+++ src/indexref.cpp
@@ -1,12 +1,13 @@
 #include "indexref.h"
 #include <stdlib.h>
+#include <iostream>
 //<m|P|n>=<m|P^{+}|n>=<n|P|m>*
 int findindex(int m,int n,int band){
   //enforce m<n
   if(m>=n){
     std::cout<<"Call Inner Product function Wrong, should always enforce m<n"<<std::endl;
     std::cout<<" if you want to see the m>=n situation, please use complex conjugate"<<std::endl;
-    EXIT(EXIT_FAILURE);
+    exit(EXIT_FAILURE);
   }
   else{
     return (2*band-m-1)*m/2+(n-m)-1;
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <sstream>
 #include <iostream>
+#include "indexref.h"
 int main(){
   std::fstream fs;
   fs.open("pmat.dat",std::fstream::in);
@@ -17,8 +18,23 @@ int main(){
     ss>>bandnumber;
   }
   std::cout<<"the kpoints are: "<<kpoints<<" the band numbers are:  "<<bandnumber<<std::endl;
-  double** kpoint_product=new double* [kpoints];
-  for(size_t i=0;i<kpoints;i++){
-    kpoint_product[i]=new double[bandnumber*(bandnumber-1)/2];
+  double*** kpoint_product=new double** [3];
+  for(size_t i=0;i<3;i++){
+    kpoint_product[i]=new double* [kpoints];
+  }
+  for(size_t i=0;i<3;i++){
+    for(size_t j=0;j<kpoints;j++){
+      kpoint_product[i][j]=new double[bandnumber*(bandnumber-1)/2];
+    }
+  }
+
+  /*deallocate memory*/
+  for(size_t i=0;i<3;i++){
+    for(size_t j=0;j<kpoints;j++){
+      delete [] kpoint_product[i][j];
+    }
+  }
+  for(size_t i=0;i<3;i++){
+    delete [] kpoint_product[i];
   }
 }
