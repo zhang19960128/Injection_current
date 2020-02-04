966c795be1dd1a6c25de3333e6de01ae29c08602
zhang19960128 <zjh199601283492@gmail.com>
2020-02-04 17:30:28 -0500

    visualize

---
+++ visual/showspectrum.m
@@ -0,0 +1,10 @@
+data=importdata("injection.dat");
+xgrid=data(1:end,1);
+xxz=data(1:end,2);
+yxz=data(1:end,3);
+zxz=data(1:end,4);
+plot(xgrid,xxz,'-r+',xgrid,yxz,'-kp',xgrid,zxz,'-m+','LineWidth',2)
+legend("xxz","yxz","zxz")
+xlabel("Photon Energy(ev)");
+ylabel("Susceptibility(A/V^2)")
+set(gca,"FontSize",26)
