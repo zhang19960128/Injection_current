2bce466f2dc6314bc3d05d2a3a6d615f403c4cf4
zhang19960128 <zjh199601283492@gmail.com>
2020-02-05 15:38:06 -0500

    change

--- visual/showspectrum.m
+++ visual/showspectrum.m
@@ -3,6 +3,7 @@ xgrid=data(1:end,1);
 xxz=data(1:end,2);
 yxz=data(1:end,3);
 zxz=data(1:end,4);
+figure('units','normalized','outerposition',[0 0 1 1])
 plot(xgrid,xxz,'-r+',xgrid,yxz,'-kp',xgrid,zxz,'-m+','LineWidth',2)
 legend("xxz","yxz","zxz")
 xlabel("Photon Energy(ev)");
