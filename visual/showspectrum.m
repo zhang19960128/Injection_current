d6516244f2a878dca8210e861b53dd26e1b123c1
zhang19960128 <zjh199601283492@gmail.com>
2020-02-05 15:54:19 -0500

    change

--- visual/showspectrum.m
+++ visual/showspectrum.m
@@ -1,12 +1,12 @@
 data=importdata("injection.dat");
 xgrid=data(1:end,1);
-xxz=data(1:end,2);
-yxz=data(1:end,3);
-zxz=data(1:end,4);
+xxz=data(1:end,2)*10^(6);
+yxz=data(1:end,3)*10^(6);
+zxz=data(1:end,4)*10^(6);
 figure('units','normalized','outerposition',[0 0 1 1])
 plot(xgrid,xxz,'-r+',xgrid,yxz,'-kp',xgrid,zxz,'-m+','LineWidth',2)
 legend("xxz","yxz","zxz")
 xlabel("Photon Energy(ev)");
-ylabel("Susceptibility(A/V^2)")
+ylabel("Susceptibility($\mu$A/$V^{2}$)",'Interpreter','latex')
 set(gca,"FontSize",26)
 saveas(gcf,'BTO.eps','epsc')
